3aa68ab4fd66ab2aad3cd7bbfc9354f91e302f11
Eitan Suez <eitan.suez@gmail.com>
2016-07-16 15:54:19 -0500

    upgraded and tested service-registry, fortune-service, and greeting-service to use spring boot 1.3.x and the brixton version of spring cloud

--- fortune-service/src/main/java/io/pivotal/FortuneServiceApplication.java
+++ fortune-service/src/main/java/io/pivotal/FortuneServiceApplication.java
@@ -11,6 +11,5 @@ public class FortuneServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(FortuneServiceApplication.class, args);
     }
-    
-    
+
 }
--- greeting-service/src/main/java/io/pivotal/greeting/GreetingController.java
+++ greeting-service/src/main/java/io/pivotal/greeting/GreetingController.java
@@ -1,5 +1,7 @@
 package io.pivotal.greeting;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,46 +10,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.DiscoveryClient;
-
 @Controller
 public class GreetingController {
 
-	Logger logger = LoggerFactory
-			.getLogger(GreetingController.class);
+  Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
-	
+  @Autowired
+  private EurekaClient discoveryClient;
 
-	
-	@Autowired
-	private DiscoveryClient discoveryClient;
-	
-	@RequestMapping("/")
-	String getGreeting(Model model){
-        
-		logger.debug("Adding greeting");
-		model.addAttribute("msg", "Greetings!!!");
-			
-		
-		RestTemplate restTemplate = new RestTemplate();
-        String fortune = restTemplate.getForObject(fetchFortuneServiceUrl(), String.class);
+  @RequestMapping("/")
+  String getGreeting(Model model) {
 
-		logger.debug("Adding fortune");
-		model.addAttribute("fortune", fortune);
-		
-		//resolves to the greeting.vm velocity template
-		return "greeting";
-	}
-	
-	private String fetchFortuneServiceUrl() {
-	    InstanceInfo instance = discoveryClient.getNextServerFromEureka("FORTUNE-SERVICE", false);
-	    logger.debug("instanceID: {}", instance.getId());
+    logger.debug("Adding greeting");
+    model.addAttribute("msg", "Greetings!!!");
 
-	    String fortuneServiceUrl = instance.getHomePageUrl();
-		logger.debug("fortune service homePageUrl: {}", fortuneServiceUrl);
+    RestTemplate restTemplate = new RestTemplate();
+    String fortune = restTemplate.getForObject(fetchFortuneServiceUrl(), String.class);
 
-	    return fortuneServiceUrl;
-	}	
-	
+    logger.debug("Adding fortune");
+    model.addAttribute("fortune", fortune);
+
+    //resolves to the greeting.vm velocity template
+    return "greeting";
+  }
+
+  private String fetchFortuneServiceUrl() {
+    InstanceInfo instance = discoveryClient.getNextServerFromEureka("FORTUNE-SERVICE", false);
+    logger.debug("instanceID: {}", instance.getId());
+
+    String fortuneServiceUrl = instance.getHomePageUrl();
+    logger.debug("fortune service homePageUrl: {}", fortuneServiceUrl);
+
+    return fortuneServiceUrl;
+  }
+
 }
